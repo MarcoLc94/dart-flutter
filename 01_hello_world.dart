8f7be9cd352129784f4ab429734c244a8e382075
marco <marcoantoniolopezcastro>
2024-12-24 10:25:19 -0600

    first commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,43 @@
+void main(){
+  print ("""
+    $pokemon
+    $hp
+    $isAlive
+    $abilities
+    $sprites
+""");
+}
+
+
+var name = "voyager";
+var year = 1977;
+var number = 2000;
+var antenna = 3.7;
+
+double pi = 3.14;
+String name2 = "marco";
+int myAge = 23;
+
+var flying = ["jupiter", "saturn"];
+List<String> letters = ["a", "b", "c"];
+List<int> numbers = [1,2,3,4,5];
+
+var image = {
+    "tags": ["saturn"], 
+    "url": "//path/saturn.jpg"
+};
+
+Map<String, String> objeto = {
+    "hola": "puto",
+    "joto": "culo"
+};
+
+int year2 = 9;
+
+final String pokemon = "ditto";
+final int hp = 100;
+final bool isAlive = true;
+final List<String> abilities = ["impostor"];
+final sprites = <String>["ditto/front.png", "ditto/back.png"];
+
+
